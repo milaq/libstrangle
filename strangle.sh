07a108731d674ffd58fe25ed589f09834b934eb5
pyamsoft <pyam.soft@gmail.com>
2016-11-04 23:26:30 -0700

    Change indent from tab to 2 spaces
    While there is no conventional spacing, it seems like many people are used to
    (or prefer) two spaces for shell script indents

--- strangle.sh
+++ strangle.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 if [ $# -eq 0 ]; then
-	echo "No arguments"
-	exit
+  echo "No arguments"
+  exit
 fi
 
 if [ "$1" -eq "$1" ] 2>/dev/null; then
-	# Check if the first command line argument is a valid number
-	FPS=$1
-	shift
+  # Check if the first command line argument is a valid number
+  FPS=$1
+  shift
 else
-	if ! [ "$FPS" -eq "$FPS" ] 2>/dev/null; then
-		# Check if the environmental variable FPS is a valid number
-		FPS=0
-	fi
+  if ! [ "$FPS" -eq "$FPS" ] 2>/dev/null; then
+    # Check if the environmental variable FPS is a valid number
+    FPS=0
+  fi
 fi
 
 export FPS="${FPS}"
