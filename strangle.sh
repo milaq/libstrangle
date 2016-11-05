bd543a46457e3f0fe1f07f921aac7ffb73e32a88
torkel104 <bjorn.spindel@protonmail.com>
2016-11-05 21:19:29 +0100

    Small fixes
    Move argument count check to account for "shift"
    Clarify error/help message

--- strangle.sh
+++ strangle.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-if [ "$#" -eq 0 ]; then
-  echo "ERROR: No arguments"
-  echo
-  echo "strangle.sh <fps-limit> <program-to-strangle>"
-  echo
-  echo "The fps-limit argument is required, and is expected to be an"
-  echo "integer value. If it is not provided, libstrangle will assume"
-  echo "there is no FPS limit"
-  exit
-fi
-
 if [ "$1" -eq "$1" ] 2>/dev/null; then
   # Check if the first command line argument is a valid number
   FPS="$1"
@@ -20,6 +9,19 @@ else
     # Check if the environmental variable FPS is a valid number
     FPS=0
   fi
+fi
+
+if [ "$#" -eq 0 ]; then
+  programname=`basename "$0"`
+  echo "ERROR: No program supplied"
+  echo
+  echo "Usage: $programname [fps-limit] <program-to-strangle>"
+  echo
+  echo "  fps-limit"
+  echo "        an integer describing the desired maximum frame rate"
+  echo "        If missing: the environment variable \"FPS\" will be used."
+  echo "        If neither is set no limit will be applied"
+  exit 1
 fi
 
 # Execute the strangled program under a clean environment
